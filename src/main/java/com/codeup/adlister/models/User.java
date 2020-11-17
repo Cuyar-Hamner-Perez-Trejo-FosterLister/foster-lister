0b4ae27309c0ce35808a7388bd90bb1ff5ae663a
Unknown <blossomperez18@gmail.com>
2020-11-17 11:21:39 -0600

    Update User dao

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -4,23 +4,55 @@ import com.codeup.adlister.util.Password;
 
 public class User {
     private long id;
-    private String username;
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private int numberOfPets;
+    private String imgURL;
+    private boolean isAdmin;
+
 
     public User() {}
 
-    public User(String username, String email, String password) {
-        this.username = username;
+    public User(String email, String password) {
         this.email = email;
         setPassword(password);
     }
 
-    public User(long id, String username, String email, String password) {
+//    public User(String email, String password, String firstName, String lastName, String address, String phoneNumber, int numberOfPets, String imgURL) {
+//        this.email = email;
+//        this.password = password;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.address = address;
+//        this.phoneNumber = phoneNumber;
+//        this. numberOfPets = numberOfPets;
+//        this.imgURL = imgURL;
+//    }
+    public User(long id, String email, String password, String firstName, String lastName, String address, String phoneNumber, int numberOfPets) {
         this.id = id;
-        this.username = username;
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this. numberOfPets = numberOfPets;
+    }
+    public User(long id,  String email, String password, String firstName, String lastName, String address, String phoneNumber, int numberOfPets, String imgURL, boolean isAdmin) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this. numberOfPets = numberOfPets;
+        this.imgURL = imgURL;
+        this.isAdmin = isAdmin;
     }
 
     public long getId() {
@@ -29,14 +61,6 @@ public class User {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getEmail() {
@@ -53,5 +77,61 @@ public class User {
 
     public void setPassword(String password) {
         this.password = Password.hash(password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNumberOfPets() {
+        return numberOfPets;
+    }
+
+    public void setNumberOfPets(int numberOfPets) {
+        this.numberOfPets = numberOfPets;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
