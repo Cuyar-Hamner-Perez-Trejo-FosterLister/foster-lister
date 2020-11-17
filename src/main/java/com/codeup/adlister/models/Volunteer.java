bb858f03a237b1e5e0b77385d7ae444a8f54e2a7
Unknown <blossomperez18@gmail.com>
2020-11-17 15:21:24 -0600

    Create constructors

--- src/main/java/com/codeup/adlister/models/Volunteer.java
+++ src/main/java/com/codeup/adlister/models/Volunteer.java
@@ -1,6 +1,29 @@
 package com.codeup.adlister.models;
 
 public class Volunteer {
+    private long id;
+    private long userId;
+    private String date;
+    private String title;
+    private String description;
+    private String contact;
+
+    public Volunteer(long id, long userId, String date, String title, String description, String contact) {
+        this.id = id;
+        this.userId = userId;
+        this.date = date;
+        this.title = title;
+        this.description = description;
+        this.contact = contact;
+    }
+
+    public Volunteer(long userId, String date, String title, String description, String contact) {
+        this.userId = userId;
+        this.date = date;
+        this.title = title;
+        this.description = description;
+        this.contact = contact;
+    }
 
 
 }
