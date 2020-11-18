10601bdeec3d379c0fc9c0a4b1ed266d9826e5e5
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-18 08:15:37 -0600

    feat: refactor Listing class for updated data structure

--- src/main/java/com/codeup/adlister/models/Listing.java
+++ src/main/java/com/codeup/adlister/models/Listing.java
@@ -3,20 +3,55 @@ package com.codeup.adlister.models;
 public class Listing {
     private long id;
     private long userId;
-    private String title;
+    private String imageUrl;
+    private String name;
+    private String type;
+    private String breed;
+    private String dob;
+    private Character gender;
+    private String conditions;
     private String description;
+    private String size;
+    private int litterSize;
+    private String fosterDuration;
+    private String createdTime;
+    private int roleId;
 
-    public Listing(long id, long userId, String title, String description) {
+    // SELECT constructor
+    public Listing(long id, long userId, String imageUrl, String name, String type, String breed, String dob, Character gender, String conditions, String description, String size, int litterSize, String fosterDuration, String createdTime, int roleId) {
         this.id = id;
         this.userId = userId;
-        this.title = title;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.type = type;
+        this.breed = breed;
+        this.dob = dob;
+        this.gender = gender;
+        this.conditions = conditions;
         this.description = description;
+        this.size = size;
+        this.litterSize = litterSize;
+        this.fosterDuration = fosterDuration;
+        this.createdTime = createdTime;
+        this.roleId = roleId;
     }
 
-    public Listing(long userId, String title, String description) {
+    // INSERT constructor
+    public Listing(long userId, String imageUrl, String name, String type, String breed, String dob, Character gender, String conditions, String description, String size, int litterSize, String fosterDuration, String createdTime, int roleId) {
         this.userId = userId;
-        this.title = title;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.type = type;
+        this.breed = breed;
+        this.dob = dob;
+        this.gender = gender;
+        this.conditions = conditions;
         this.description = description;
+        this.size = size;
+        this.litterSize = litterSize;
+        this.fosterDuration = fosterDuration;
+        this.createdTime = createdTime;
+        this.roleId = roleId;
     }
 
     public long getId() {
@@ -35,12 +70,60 @@ public class Listing {
         this.userId = userId;
     }
 
-    public String getTitle() {
-        return title;
+    public String getImageUrl() {
+        return imageUrl;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
     }
 
     public String getDescription() {
@@ -49,5 +132,45 @@ public class Listing {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getLitterSize() {
+        return litterSize;
+    }
+
+    public void setLitterSize(int litterSize) {
+        this.litterSize = litterSize;
+    }
+
+    public String getFosterDuration() {
+        return fosterDuration;
+    }
+
+    public void setFosterDuration(String fosterDuration) {
+        this.fosterDuration = fosterDuration;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
     }
 }
